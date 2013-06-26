eef6f3e493e85c5f314bdddee3e9b553dba0b17e
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2013-06-26 19:48:04 -0400

    Add my code to exercises

--- Lesson02/src/IntArraySorter.java
+++ Lesson02/src/IntArraySorter.java
@@ -1,10 +1,27 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Trinitee
+ * Date: 6/24/13
+ * Time: 6:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
 public class IntArraySorter {
     public int[] sortArray(int[] array) {
-        // TODO:
         // For each number in the array
         // Look at every number after it, in sequence
         // If the first number is greater than the second, swap them
+        int placeholder = 0;
 
+        for (int i = 0; i < array.length; i++ ){
+            for (int x = i + 1; x < array.length; x++ ){
+                if(array[i] > array[x]) {
+                    placeholder = array[i];
+                    array[i] = array[x];
+                    array[x] = placeholder;
+
+                }
+            }
+        }
         return array;
     }
 }
--- Lesson02/src/Lesson02Samples.java
+++ Lesson02/src/Lesson02Samples.java
@@ -1,4 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Trinitee
+ * Date: 6/24/13
+ * Time: 6:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lesson02Samples {
@@ -11,7 +20,7 @@ public class Lesson02Samples {
         //reverseNumbers();
         //reverseArray();
         //isGeneticSequence();
-        //inputAndSearch();
+        inputAndSearch();
         //calculator();
     }
 
@@ -40,43 +49,146 @@ public class Lesson02Samples {
 
     // There's a bug in this, find it
     public static boolean stringIsAllAs(String s) {
-        int i = -1;
-        while (i <= s.length()) {
-            i++;
+        int i = 0;
+        while (i < s.length()) {
+
             char character = s.charAt(i);
             if (character == 'a' || character == 'A') {
-                return true;
+
+                i++;
             }
             else {
+
                 return false;
             }
         }
         return true;
+
     }
 
     // Methods to fill in
     public static void reverseNumbers() {
+
         // 1) Ask the user for how many numbers they want to enter
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        System.out.println("How many numbers do you want to enter?:" );
+        Integer response = scanner.nextInt();
+
         // 2) Let them enter that many numbers
+        int x = 0;
+        while (x < response){
+            System.out.println(" Enter a number ");
+            numbers.add(scanner.nextInt());
+            x++;
+
+        }
+
         // 3) Print them out in reverse
+        for(int i = numbers.size() - 1; i >= 0; i--){
+            System.out.println(numbers.get(i));
+        }
     }
 
     public static void reverseArray() {
-        int[] array = {12, 320, 231, 0214, 12312, 41024, 124, 12, 4123, 123, 1234, 634, 85, 685, 68};
+        int[] array = {12, 320, 231, 214, 12312, 41024, 124, 12, 4123, 123, 1234, 634, 85, 685, 87};
         // 1) Reverse the above array above without creating a new array, then print it out
+        int placeholder = 0;
+        int n = array.length;
+
+          //this is my original solution that I refactored below
+//        if(n % 2 == 0){
+//            for(int i = 1; i < n / 2 + 1; i++){
+//                if(i == n / 2 ){
+//                    placeholder = array[i - 1];
+//                    array[i - 1] = array[n - i];
+//                    array[n - i] = placeholder;
+//                    i += 2;
+//                }
+//                else{
+//                    placeholder = array[i - 1];
+//                    array[i - 1] = array[n - i];
+//                    array[n - i] = placeholder;
+//                }
+//            }
+//        }
+//        else {
+//            for(int i = 1; i < (n / 2) + 1; i++){
+//                placeholder = array[i - 1];
+//                array[i - 1] = array[n - i];
+//                array[n - i] = placeholder;
+//            }
+//
+//        }
+
+        for(int i = 1; i < n / 2 + 1; i++){
+
+            placeholder = array[i - 1];
+            array[i - 1] = array[n - i];
+            array[n - i] = placeholder;
+
+            //Below is added so that for even numbered arrays, the middle 2 numbers will not be swapped
+            if((n % 2 == 0) && (i == n / 2)){
+                i += 2;
+            }
+
+        }
+
+        for(int x = 0; x < n; x++ ){
+            System.out.println(array[x]);
+        }
     }
 
     public static void isGeneticSequence() {
         // 1) Ask the user for a string
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter any line of text that you wish: ");
+        String response = scanner.next();
         // 2) If the string contains only the characters 'G', 'A', 'T', or 'C' (case-sensitive) print true
+        int i = 0;
+        while(i < response.length()){
+           //if(response.charAt(i) isin ('G', 'A', 'T', 'C')){
+        //}
+
         // 3) Otherwise, print false
+        }
     }
 
     public static void inputAndSearch() {
         // 1) Ask the user to input 10 words
+        ArrayList<String> words = new ArrayList<String>();
+        Scanner scanner =   new Scanner(System.in);
+
+        for (int i = 1; i <= 2; i++){
+            System.out.println("Please enter a word: ");
+            words.add(scanner.next());
+        }
+
         // 2) Then ask the user to input a search word
+        String searchAgain = "yes";
+
+        while(searchAgain.equals("yes")){
+            Boolean isThere = false;
+            System.out.println("Please enter a search word");
+            String searchWord = scanner.next();
         // 3) If the word exists in the first 10 words, print true, otherwise print false
+
+            int x = 0;
+            while((x < words.size()) && (isThere == false)){
+                if(words.get(x).equals(searchWord)){
+                   isThere = true;
+                   x = words.size();
+                }
+                else{
+                    x += 1;
+                }
+
+            }
+            System.out.println("The word " + searchWord + " is in the list? " + isThere);
         // 4) Return to step 2
+            System.out.println("Would you like to search for another word(yes/no)? ");
+            searchAgain = scanner.next().trim();
+        }
     }
 
     public static void calculator() {
@@ -84,11 +196,20 @@ public class Lesson02Samples {
         // 1) Modify it to support both addition, subtraction and multiplication operations
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.print("Enter an addition statement in the format x + y: ");
+            System.out.print("Enter an mathematical statement in the format x + y. Use +, -, *(for multiply): ");
             Integer leftOperand = scanner.nextInt();
-            String plus = scanner.next(); // The "+" sign, just ignore this
+            String operator = scanner.next(); // The "+" sign, just ignore this
             Integer rightOperand = scanner.nextInt();
-            System.out.println("Resulting value is: " + (leftOperand.intValue() + rightOperand.intValue()));
+            if(operator.equals("+")){
+                System.out.println("Resulting value is: " + (leftOperand.intValue() + rightOperand.intValue()));
+            }
+            else if(operator.equals("-")){
+                System.out.println("Resulting value is: " + (leftOperand.intValue() - rightOperand.intValue()));
+
+            }
+            else if(operator.equals("*")){
+                System.out.println("Resulting value is: " + (leftOperand.intValue() * rightOperand.intValue()));
+            }
         }
     }
 }
